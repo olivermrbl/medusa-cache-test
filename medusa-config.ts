298cd8d0952d167b0b11b3ab5cdbe1a41ae3a8a9
olivermrbl <oliver@mrbltech.com>
2025-10-14 10:48:53 +0200

    chore: Add explicit cache configuration

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,6 +1,6 @@
-import { defineConfig, loadEnv } from '@medusajs/framework/utils'
+import { defineConfig, loadEnv } from "@medusajs/framework/utils";
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
 module.exports = defineConfig({
   projectConfig: {
@@ -11,9 +11,26 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+    },
   },
+  modules: [
+    {
+      resolve: "@medusajs/medusa/caching",
+      options: {
+        providers: [
+          {
+            id: "caching-redis",
+            resolve: "@medusajs/caching-redis",
+            is_default: true,
+            options: {
+              redisUrl: process.env.REDIS_URL,
+            },
+          },
+        ],
+      },
+    },
+  ],
   featureFlags: {
-    caching: true
-  }
-})
+    caching: true,
+  },
+});
